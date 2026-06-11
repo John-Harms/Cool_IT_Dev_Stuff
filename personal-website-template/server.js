1cba4b0d5c3e49716560b284c8eed69ad35b0aa4
JohnHarms2023 <johnharms0404@gmail.com>
2026-06-11 17:54:00 -0500

    Part 2

--- personal-website-template/server.js
+++ personal-website-template/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const mainRouter = require("./routes/generalRoute");
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "static")));
+app.use("/pictures", express.static(path.join(__dirname, "pictures")));
 app.set("view engine", "ejs");
 app.use("/", mainRouter);
+// Serve files from your 'pictures' directory
 
-app.listen(3000, () => console.log("server ready"));
+app.listen(8040, () => console.log("server ready"));
